b61df8086208b1d697aaee187f9c19bd45c13844
EgrSuleiko <Egor.Suleiko@yandex.by>
2024-12-29 22:49:47 +0300

    Final version

--- main.py
+++ main.py
@@ -1,2 +1,47 @@
 def main ():
-    print('Hello')
+    result = calculator(*unpack_string(input()))
+    print(result)
+
+
+def unpack_string(str):
+    if len(str.split()) != 3:
+        raise ValueError('Cтрока не является математической операцией')
+
+    num1, operand, num2 = str.split()
+
+    if operand not in ['+', '-', '*', '/']:
+        raise ValueError('Введена неверная операция')
+
+    if not is_int_number(num1) or not is_int_number(num2):
+        raise ValueError('Одно или несколько введенные значений не является целым числом')
+
+    return int(num1), int(num2), operand
+
+
+def is_int_number(str):
+    try:
+        int(str)
+        return True
+    except ValueError:
+        return False
+
+
+def calculator(num1, num2, operand):
+    if num1 < 1 or num2 < 1:
+        raise ValueError('Одно или несколько чисел меньше 1, что не удовлетворяет условиям задачи')
+
+    if num1 > 10 or num2 > 10:
+        raise ValueError('Одно или несколько чисел больше 10, что не удовлетворяет условиям задачи')
+
+    match operand:
+        case '+':
+            return num1 + num2
+        case '-':
+            return num1 - num2
+        case '*':
+            return num1 * num2
+        case '/':
+            return num1 // num2
+
+
+main()
